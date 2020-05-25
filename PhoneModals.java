eeded85a5001b6ccb54df4de1c68dd329323faa4
aakannan7 <65912610+aakannan7@users.noreply.github.com>
2020-05-25 21:34:40 +0530

    Initial Commit

---
+++ PhoneModals.java
@@ -0,0 +1,11 @@
+public class NewPhones{
+
+	public static void main(String[] args) {
+		
+		System.out.println("Samsung");
+		System.out.println("Oneplus");
+		System.out.println("RedMi");
+		System.out.println("Realme");
+		
+}
+}
